a38b835ae1dbf69fc0452d9ae5c6789861dac3fe
Sachie Tran <trantran085@gmail.com>
2018-04-23 16:05:38 -0400

    Update lex-scan-parse.scm

--- lex-scan-parse.scm
+++ lex-scan-parse.scm
@@ -2,9 +2,7 @@
 ;; Contains lexical specification and code that generates the scanner
 ;; and parser from the SLLGEN library.
 
-;; You only need the parse function that this code define.
 
-;; WARNING: Do NOT modify this file unless explicitly instructed to do so.
 
 ;; ================ Scanner Definitions ==================================
 
@@ -34,8 +32,7 @@
   )
 
 ;; ================= SLLGEN Boilerplate ==================================
-;; WARNING: Presumes a variable the-grammar has be created that stores the
-;; mapping between the concrete and abstract syntax.
+
   
 ;; This command makes the abstract grammar from the description in the-grammar
 ;; With this you don't need to use define-datatypes
